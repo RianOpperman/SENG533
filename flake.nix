a8f9436e490932e37ad5b79af991c6d8128ad4ae
ILLEIST <rlopperman@proton.me>
2025-03-30 19:49:58 -0600

    Init commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Basic template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" ];
+      forEachSystem = f: nixpkgs.lib.genAttrs  systems (system: f system);
+    in
+  {
+    devShells = forEachSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        mkScript = name: text: let
+          script = pkgs.writeShellScriptBin name text;
+        in script;
+        scripts = [
+          (mkScript "up" ''docker compose up -d'')
+          (mkScript "down" ''docker compose down --timeout 0'')
+        ];
+      in {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            zulu
+            maven
+            kubernetes
+            minikube
+            kubernetes-helm
+          ] ++ scripts;
+        };
+      });
+  };
+}
